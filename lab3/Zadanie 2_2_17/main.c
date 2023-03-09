03dda8f0265c49d5fd7639f4629c4447a28aa3de
MichalinaSobkiewicz <sobkiewiczmichalina@gmail.com>
2023-03-09 13:42:28 +0100

    Update main.c

--- lab3/Zadanie 2_2_17/main.c
+++ lab3/Zadanie 2_2_17/main.c
@@ -1,16 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int main()
+{
 
-void count_calls() {
-    static int num_calls = 0;
-    num_calls++;
-    printf("Function has been called %d times.\n", num_calls);
-}
-
-int main() {
-    count_calls(); // Function has been called 1 times.
-    count_calls(); // Function has been called 2 times.
-    count_calls(); // Function has been called 3 times.
-    return 0;
 }
